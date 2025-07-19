8c6ead8e618672a5df09f406101aa1a070100a60
Nikita <bansalnikita06@gmail.com>
2025-07-19 19:05:46 +0530

    update package-lock.json

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-
-// run async setup but don't await it here
-if (process.env.NODE_ENV === "development") {
-  setupDevPlatform().catch(console.error);
-}
 
 const nextConfig: NextConfig = {
-  // your config here
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
