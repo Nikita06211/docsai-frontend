cbf36d7b6b28d497bdfe85b340000ca0db1e39cb
Nikita <bansalnikita06@gmail.com>
2025-07-19 16:55:24 +0530

    next.config fixes

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
+// run async setup but don't await it here
+if (process.env.NODE_ENV === "development") {
+  setupDevPlatform().catch(console.error);
+}
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // your config here
 };
-
- if (process.env.NODE_ENV === 'development') {
-   await setupDevPlatform();
- }
 
 export default nextConfig;
