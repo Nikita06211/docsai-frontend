b3408b03674ca420a9bf3f5fc62aa324dca74a9a
Nikita <bansalnikita06@gmail.com>
2025-07-27 20:03:56 +0530

    added registerLink

--- src/app/components/AuthForm.tsx
+++ src/app/components/AuthForm.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/components';
+import { RegisterLink } from '@kinde-oss/kinde-auth-nextjs/components';
 import React from 'react';
 
 
 export default function AuthButtons() {
   return (
-    <LoginLink className="hover:bg-green-400 hover:text-black border-1 rounded-lg px-3 py-2 mt-2 bg-white text-green-400 block text-center cursor-pointer">
+    <RegisterLink className="hover:bg-green-400 hover:text-black border-1 rounded-lg px-3 py-2 mt-2 bg-white text-green-400 block text-center cursor-pointer">
       {'>'} ACCESS_TERMINAL
-  </LoginLink>
+  </RegisterLink>
 
   );
 }
