0c3fca64780d37f92df863a9dd356d8cc19231b2
Nikita <bansalnikita06@gmail.com>
2025-07-28 18:50:34 +0530

    update AuthButtons styling and implement post-login page with chat functionality

--- src/app/components/AuthForm.tsx
+++ src/app/components/AuthForm.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 
 export default function AuthButtons() {
   return (
-    <RegisterLink className="hover:bg-green-400 hover:text-black border-1 rounded-lg px-3 py-2 mt-2 bg-white text-green-400 block text-center cursor-pointer">
+    <RegisterLink className="hover:bg-green-400 hover:text-black border-1 rounded-lg px-3 py-2 mt-7 bg-white text-green-400 block text-center cursor-pointer">
       {'>'} ACCESS_TERMINAL
   </RegisterLink>
 
